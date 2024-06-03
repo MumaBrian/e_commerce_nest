383d6732b3d302a9fc3a5a492803c67b2a8c8b6f
MumaBrian <brianmuma5@gmail.com>
2024-06-03 08:35:18 +0100

    updated users entity table

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,12 +4,16 @@ import {
 	Post,
 	Body,
 	Param,
-	Delete,
 	Patch,
+	Delete,
+	UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { Roles } from '../common/decorators/roles.decorator';
+import { UserRole } from '../database/enums/user-role.enum';
 
 @Controller('users')
 export class UsersController {
@@ -21,21 +25,29 @@ export class UsersController {
 	}
 
 	@Get()
+	@UseGuards(JwtAuthGuard)
+	@Roles(UserRole.Admin)
 	findAll() {
 		return this.usersService.findAll();
 	}
 
 	@Get(':id')
+	@UseGuards(JwtAuthGuard)
+	@Roles(UserRole.Customer, UserRole.Admin)
 	findOne(@Param('id') id: string) {
 		return this.usersService.findOne(+id);
 	}
 
 	@Patch(':id')
+	@UseGuards(JwtAuthGuard)
+	@Roles(UserRole.Admin)
 	update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
 		return this.usersService.update(+id, updateUserDto);
 	}
 
 	@Delete(':id')
+	@UseGuards(JwtAuthGuard)
+	@Roles(UserRole.Admin)
 	remove(@Param('id') id: string) {
 		return this.usersService.remove(+id);
 	}
